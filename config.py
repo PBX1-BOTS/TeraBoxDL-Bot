1ffb3bf3e716410974e7221911723c98ec3d0ca2
PBX1-BOTS <bxjjjzid@gmail.com>
2024-07-07 03:09:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID = 20244111  # api id
-API_HASH = "b76d27da2a4220fe109fe9ef0e866530"  # api hash
+API_ID = 20841292  # api id
+API_HASH = "58c4e84b9b2506684df6447d3e324dd9"  # api hash
 
-BOT_TOKEN = "7108727290:AAEWmIXU1Z0lYiZPQBMCgKBrlNAbkY7FP2U"  # bot token
+BOT_TOKEN = "7108727290:7350485917:AAEGDgrOAuAGuZKj0czmbgwntUOgwd3aaz0"  # bot token
 
 
 ## REDIS
-HOST = "redis-15747.c274.us-east-1-3.ec2.cloud.redislabs.com"  # redis host uri
-PORT = 15747  # redis port
-PASSWORD = "YM3ciTaM5GrvydS6LzdLxY9ABgibxmz6"  # redis password
+HOST = "redis-16221.c81.us-east-1-2.ec2.redns.redis-cloud.com:16221"  # redis host uri
+PORT = 16221  # redis port
+PASSWORD = "D2hE3k3b0j8Kx4MvqpEfMeOw01scc2Jk"  # redis password
 
-PRIVATE_CHAT_ID = -1004187836496  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [5572938538]
+PRIVATE_CHAT_ID = -1002156622478  # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = "browserid=ZJYqaWZCP7jD57uTWB2MFGT-9uE- vrC6nG07rQm8Wc7cEdLJwP2iayHMPS0=; lang=en; _bid_n=1906e71c3e4a6a41944207; ndus-YVFUYK4teHui_4RhmWcqxSkdxujyrVyRamZcB61n; PANWEB-1; csrfToken=XsCAZcr0LP0mH2vce5B0t3bv; ndut_fmt=D69629668B095ED78E8BD63789C62DDF7AB1393DBD5555851C80B9EE5EC04EC1"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+ADMINS = [6052303737]
